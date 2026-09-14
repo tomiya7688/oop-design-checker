738b0697c02eaeae02d8b28c74f7b4237d3f10f2
kadoka <tomiya3499@gmail.com>
2026-09-14 23:05:14 +0900

    Apply final CSharpier catch-filter layout

--- tests/OopDesignChecker.Tests/Program.cs
+++ tests/OopDesignChecker.Tests/Program.cs
@@ -128,8 +128,7 @@ internal static class Program
                 _ = new CSharpProjectLoader().Load(sourcePath);
             }
             catch (InvalidOperationException exception)
-                when (
-                    exception.Message.Contains(
+                when (exception.Message.Contains(
                         "could not be analyzed reliably",
                         StringComparison.Ordinal
                     )
