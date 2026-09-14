b5a891db8ef293604754d920cc70b230e58d3d2e
kadoka <tomiya3499@gmail.com>
2026-09-14 22:37:04 +0900

    Keep generated project trees out of rule traversal

--- src/OopDesignChecker/Analysis/SourceProject.cs
+++ src/OopDesignChecker/Analysis/SourceProject.cs
@@ -6,17 +6,20 @@ namespace OopDesignChecker.Analysis;
 internal sealed class SourceProject
 {
     private readonly IReadOnlyDictionary<SyntaxTree, SemanticModel> _semanticModels;
+    private readonly IReadOnlyList<SyntaxTree> _syntaxTrees;
 
     public SourceProject(
         string rootPath,
         bool isApplication,
         CSharpCompilation compilation,
-        IReadOnlyDictionary<SyntaxTree, SemanticModel> semanticModels)
+        IReadOnlyDictionary<SyntaxTree, SemanticModel> semanticModels,
+        IReadOnlyList<SyntaxTree>? syntaxTrees = null)
     {
         RootPath = rootPath;
         IsApplication = isApplication;
         Compilation = compilation;
         _semanticModels = semanticModels;
+        _syntaxTrees = syntaxTrees ?? compilation.SyntaxTrees.ToArray();
     }
 
     public string RootPath { get; }
@@ -25,7 +28,7 @@ internal sealed class SourceProject
 
     public CSharpCompilation Compilation { get; }
 
-    public IEnumerable<SyntaxTree> SyntaxTrees => Compilation.SyntaxTrees;
+    public IEnumerable<SyntaxTree> SyntaxTrees => _syntaxTrees;
 
     public SemanticModel GetSemanticModel(SyntaxTree syntaxTree) => _semanticModels[syntaxTree];
 }
