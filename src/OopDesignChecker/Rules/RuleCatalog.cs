fa7c63f7cceddc8f7526f5e48b06c9d142c92ef9
kadoka <tomiya3499@gmail.com>
2026-09-14 22:06:13 +0900

    Register core OOP rule sprint

--- src/OopDesignChecker/Rules/RuleCatalog.cs
+++ src/OopDesignChecker/Rules/RuleCatalog.cs
@@ -6,13 +6,17 @@ internal static class RuleCatalog
 {
     public static IReadOnlyList<IAnalysisRule> CreateDefault() =>
     [
+        new MissingCommonAbstractionRule(),
         new TypeBranchPolymorphismRule(),
         new ExcessiveVisibilityRule(),
+        new SealingCandidateRule(),
         new StaticMemberCandidateRule(),
         new StaticClassCandidateRule(),
+        new StatefulStaticDesignRule(),
         new EncapsulationLeakRule(),
         new OversizedMainOperationRule(),
         new ChildDisablesParentBehaviorRule(),
-        new ExcessiveInheritanceDepthRule()
+        new ExcessiveInheritanceDepthRule(),
+        new ConcreteTypeDependencyRule()
     ];
 }
