c7da1ed6979df515c0dd46227ed642c12de220ee
kadoka <tomiya3499@gmail.com>
2026-09-14 21:46:58 +0900

    Expose reusable CUI entry point

---
+++ src/OopDesignChecker/CheckerCommandLine.cs
@@ -0,0 +1,24 @@
+using OopDesignChecker.Application;
+using OopDesignChecker.Output;
+
+namespace OopDesignChecker;
+
+public static class CheckerCommandLine
+{
+    public static int Run(IReadOnlyList<string> args)
+    {
+        var optionsResult = CommandLineOptionsParser.Parse(args);
+        if (!optionsResult.IsSuccess)
+        {
+            Console.Error.WriteLine(optionsResult.ErrorMessage);
+            Console.Error.WriteLine(CommandLineOptionsParser.Usage);
+            return 2;
+        }
+
+        var application = new CheckerApplication(
+            new CheckerService(),
+            new ConsoleDiagnosticWriter());
+
+        return application.Run(optionsResult.Options!);
+    }
+}
