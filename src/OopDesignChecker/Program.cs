9f140d1e71c3d70ad1ddd17fe57b481ce2fa4492
kadoka <tomiya3499@gmail.com>
2026-09-14 21:47:07 +0900

    Delegate legacy entry point to shared CUI host

--- src/OopDesignChecker/Program.cs
+++ src/OopDesignChecker/Program.cs
@@ -1,28 +1,6 @@
-using OopDesignChecker.Application;
-using OopDesignChecker.Analysis;
-using OopDesignChecker.Output;
-using OopDesignChecker.Rules;
-
 namespace OopDesignChecker;
 
 internal static class Program
 {
-    public static int Main(string[] args)
-    {
-        var optionsResult = CommandLineOptionsParser.Parse(args);
-        if (!optionsResult.IsSuccess)
-        {
-            Console.Error.WriteLine(optionsResult.ErrorMessage);
-            Console.Error.WriteLine(CommandLineOptionsParser.Usage);
-            return 2;
-        }
-
-        var loader = new CSharpProjectLoader();
-        var rules = RuleCatalog.CreateDefault();
-        var engine = new AnalysisEngine(rules);
-        var writer = new ConsoleDiagnosticWriter();
-        var application = new CheckerApplication(loader, engine, writer);
-
-        return application.Run(optionsResult.Options!);
-    }
+    public static int Main(string[] args) => CheckerCommandLine.Run(args);
 }
