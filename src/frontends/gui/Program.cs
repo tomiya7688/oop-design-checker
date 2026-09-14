0e6ee8877d7bb195cf35b104073f9f5f8c7f9bc3
kadoka <tomiya3499@gmail.com>
2026-09-14 21:50:09 +0900

    Add GUI frontend entry point

---
+++ src/frontends/gui/Program.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+
+namespace OopDesignChecker.Gui;
+
+internal static class Program
+{
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+
+    private static AppBuilder BuildAvaloniaApp() =>
+        AppBuilder.Configure<App>()
+            .UsePlatformDetect();
+}
