600bea432aeae94b51c2ab3afcc5839ec8c5254f
kadoka <tomiya3499@gmail.com>
2026-09-16 07:30:22 +0900

    Add CLI version reporting

--- src/OopDesignChecker/CheckerCommandLine.cs
+++ src/OopDesignChecker/CheckerCommandLine.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using OopDesignChecker.Application;
 using OopDesignChecker.Output;
 
@@ -7,6 +8,12 @@ public static class CheckerCommandLine
 {
     public static int Run(IReadOnlyList<string> args)
     {
+        if (args.Count == 1 && args[0] is "--version" or "-V")
+        {
+            Console.WriteLine(GetVersion());
+            return 0;
+        }
+
         var optionsResult = CommandLineOptionsParser.Parse(args);
         if (!optionsResult.IsSuccess)
         {
@@ -22,5 +29,20 @@ public static class CheckerCommandLine
         );
         var application = new CheckerApplication(diagnosticWriter);
         return application.Run(options);
+    }
+
+    private static string GetVersion()
+    {
+        var assembly = typeof(CheckerCommandLine).Assembly;
+        var informationalVersion = assembly
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+            ?.InformationalVersion;
+
+        if (!string.IsNullOrWhiteSpace(informationalVersion))
+        {
+            return informationalVersion.Split('+', 2)[0];
+        }
+
+        return assembly.GetName().Version?.ToString(3) ?? "unknown";
     }
 }
