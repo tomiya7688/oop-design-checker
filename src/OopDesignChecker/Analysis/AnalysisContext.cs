19ce30229458d0d1e15ca5f40d90f394ac8c4bca
kadoka <tomiya3499@gmail.com>
2026-09-16 10:29:36 +0900

    Finish CSharpier formatting for analysis context

--- src/OopDesignChecker/Analysis/AnalysisContext.cs
+++ src/OopDesignChecker/Analysis/AnalysisContext.cs
@@ -3,7 +3,5 @@ namespace OopDesignChecker.Analysis;
 internal sealed record AnalysisContext(SourceProject Project, IReadOnlyList<SourceProject> Projects)
 {
     public AnalysisContext(SourceProject project)
-        : this(project, [project])
-    {
-    }
+        : this(project, [project]) { }
 }
