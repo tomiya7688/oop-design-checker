9c1c6f524897583f5f204329546e98aae195fc5c
kadoka <tomiya3499@gmail.com>
2026-09-17 21:47:27 +0900

    Use Japanese-first CUI localization

--- src/OopDesignChecker/CheckerCommandLine.cs
+++ src/OopDesignChecker/CheckerCommandLine.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using OopDesignChecker.Application;
+using OopDesignChecker.Localization;
 using OopDesignChecker.Output;
 
 namespace OopDesignChecker;
@@ -14,21 +15,39 @@ public static class CheckerCommandLine
             return 0;
         }
 
+        var requestedLanguage = DetectRequestedLanguage(args);
         var optionsResult = CommandLineOptionsParser.Parse(args);
         if (!optionsResult.IsSuccess)
         {
             Console.Error.WriteLine(optionsResult.ErrorMessage);
-            Console.Error.WriteLine(CommandLineOptionsParser.Usage);
+            Console.Error.WriteLine(CommandLineOptionsParser.Usage(requestedLanguage));
             return 2;
         }
 
         var options = optionsResult.Options!;
         var diagnosticWriter = DiagnosticWriterFactory.Create(
             options.OutputFormat,
-            options.OutputPath
+            options.OutputPath,
+            options.Language
         );
         var application = new CheckerApplication(diagnosticWriter);
         return application.Run(options);
+    }
+
+    private static UserInterfaceLanguage DetectRequestedLanguage(IReadOnlyList<string> args)
+    {
+        for (var index = 0; index + 1 < args.Count; index++)
+        {
+            if (
+                args[index] == "--language"
+                && UserInterfaceText.TryParseLanguage(args[index + 1], out var language)
+            )
+            {
+                return language;
+            }
+        }
+
+        return UserInterfaceText.DefaultLanguage;
     }
 
     private static string GetVersion()
