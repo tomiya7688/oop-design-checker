f5f5ca6b4c453091f2576cad4f41e90250ac76d6
kadoka <tomiya3499@gmail.com>
2026-09-19 12:39:45 +0900

    Use bundled references for loose-source analysis

--- src/OopDesignChecker/Analysis/MetadataReferenceProvider.cs
+++ src/OopDesignChecker/Analysis/MetadataReferenceProvider.cs
@@ -7,16 +7,33 @@ internal static class MetadataReferenceProvider
     public static IReadOnlyList<MetadataReference> CreatePlatformReferences()
     {
         var trustedAssemblies = AppContext.GetData("TRUSTED_PLATFORM_ASSEMBLIES") as string;
-        if (string.IsNullOrWhiteSpace(trustedAssemblies))
+        if (!string.IsNullOrWhiteSpace(trustedAssemblies))
         {
-            throw new InvalidOperationException(
-                "The runtime did not provide trusted platform assemblies."
-            );
+            return trustedAssemblies
+                .Split(Path.PathSeparator, StringSplitOptions.RemoveEmptyEntries)
+                .Select(path => MetadataReference.CreateFromFile(path))
+                .ToArray();
         }
 
-        return trustedAssemblies
-            .Split(Path.PathSeparator, StringSplitOptions.RemoveEmptyEntries)
-            .Select(path => MetadataReference.CreateFromFile(path))
-            .ToArray();
+        var bundledReferenceDirectory = Path.Combine(
+            AppContext.BaseDirectory,
+            "ReferenceAssemblies"
+        );
+        if (Directory.Exists(bundledReferenceDirectory))
+        {
+            var bundledReferences = Directory
+                .EnumerateFiles(bundledReferenceDirectory, "*.dll", SearchOption.TopDirectoryOnly)
+                .OrderBy(path => path, StringComparer.Ordinal)
+                .Select(path => MetadataReference.CreateFromFile(path))
+                .ToArray();
+            if (bundledReferences.Length > 0)
+            {
+                return bundledReferences;
+            }
+        }
+
+        throw new InvalidOperationException(
+            "The runtime did not provide trusted platform assemblies and no bundled reference assemblies were found."
+        );
     }
 }
