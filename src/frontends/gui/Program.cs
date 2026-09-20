5ad2fdde8d810ae7c57c33c786d1485e722afae7
kadoka <tomiya3499@gmail.com>
2026-09-20 18:18:18 +0900

    Expose GUI product version

--- src/frontends/gui/Program.cs
+++ src/frontends/gui/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Avalonia;
 
 namespace OopDesignChecker.Gui;
@@ -5,8 +6,21 @@ namespace OopDesignChecker.Gui;
 internal static class Program
 {
     [STAThread]
-    public static void Main(string[] args) =>
+    public static void Main(string[] args)
+    {
+        if (args.Length == 1 && args[0] is "--version" or "-V")
+        {
+            Console.WriteLine(GetProductVersion());
+            return;
+        }
+
         BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
+
+    private static string GetProductVersion() =>
+        typeof(Program).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
+        ?? typeof(Program).Assembly.GetName().Version?.ToString(3)
+        ?? "unknown";
 
     private static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>().UsePlatformDetect();
 }
